13d10d9c36dd28c6f6de6bdd42bf3c48a3a9e1e4
Strashimir Yosifov <syosifov@gmail.com>
2022-03-24 11:00:27 +0200

    V0

---
+++ src/main/java/net/yosifov/accounting/accj/accj2config/Accj2ConfigApplication.java
@@ -0,0 +1,13 @@
+package net.yosifov.accounting.accj.accj2config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Accj2ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Accj2ConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/yosifov/accounting/accj/accj2config/Accj2ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yosifov.accounting.accj.accj2config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Accj2ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
