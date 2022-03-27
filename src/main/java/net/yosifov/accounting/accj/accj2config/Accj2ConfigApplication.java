e6b30a85179107878c9a4040cdeacf850ae2994b
Strashimir Yosifov <syosifov@gmail.com>
2022-03-27 12:11:18 +0300

    Config example installed

--- src/main/java/net/yosifov/accounting/accj/accj2config/Accj2ConfigApplication.java
+++ src/main/java/net/yosifov/accounting/accj/accj2config/Accj2ConfigApplication.java
@@ -2,7 +2,9 @@ package net.yosifov.accounting.accj.accj2config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class Accj2ConfigApplication {
 
